13a3f28242d32baf2782b72a6878b4ef8492dd86
Chloe Antonio <chantoni@iu.edu>
2021-10-11 11:36:52 -0400

    Update model.js

--- app/model.js
+++ app/model.js
@@ -1,6 +1,6 @@
 var MODEL = (function () {
   var _grabNewContent = function (pageName) {
-    $.get(`../pages/${pageName}/${pageName}.html`, function (data) {
+    $.get(`pages/${pageName}/${pageName}.html`, function (data) {
       console.log(data);
 
       $(".app").html(data);
